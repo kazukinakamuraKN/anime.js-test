aeb39c8d775dd240e05fb28923f658a41f2f426a
Kazuki Nakamura <kajirikajiri@gmail.com>
2018-12-23 11:41:22 +0900

    ah

--- src/index.js
+++ src/index.js
@@ -1,12 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import anime from 'animejs'
+import Anime from 'react-anime'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Game extends React.Component {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  render() {
+    return (
+      <Anime easing="easeOutElastic"
+         duration={2000}
+         direction="alternate"
+         loop={true}
+         delay={(el, index) => index * 240}
+         translateX='0rem'
+         translateY='0rem'
+         rotate='1turn'
+         scale={[.75, .9]}
+         backgroundColor='#FF0'
+         border-radius='50%'>
+      <div className="blue">
+      <Anime easing="easeOutElastic"
+         duration={1300}
+         direction="alternate"
+         loop={true}
+         delay={(el, index) => index * 10}
+         translateX='0rem'
+         translateY='0rem'
+         rotate='1turn'
+         scale={[.75, .9]}
+         backgroundColor='#FFF'
+         border-radius='50%'>
+        <div className="green">
+          
+         <div className="red">
+         </div>
+         
+        </div>
+        </Anime>
+      </div>
+      </Anime>
+    )
+  }
+}
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
